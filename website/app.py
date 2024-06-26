202ce02959331f6cf678f01310ff521debae667a
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2024-06-26 15:43:01 +0900

    adding challenge endpoints

--- website/app.py
+++ website/app.py
@@ -1,21 +1,23 @@
 import os
 import sys
+import json
 import requests
 
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, url_for, request, session, current_app
 from dotenv import load_dotenv
 from urllib.parse import urlencode
 
 load_dotenv()
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder=".")
 app.secret_key = os.urandom(24)
 
-DISCORD_CLIENT_ID = os.getenv('CLIENT_ID')
-DISCORD_CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+DISCORD_CLIENT_ID = os.getenv("CLIENT_ID")
+DISCORD_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 DISCORD_REDIRECT_URI = "http://127.0.0.1:5000/callback"
 
 NUM = 2
+
 
 @app.route("/")
 def index():
@@ -26,11 +28,13 @@ def index():
         text = "Logout"
     return render_template("index.html", img=img, text=text, num=NUM)
 
+
 @app.route("/pre-login")
 def pre_login():
     if "user_data" in session:
         return render_template("logout.html", img=session["user_data"]["img"], text="")
     return render_template("login.html", img="images/index/blank.png", text="")
+
 
 @app.route("/login")
 def login():
@@ -38,62 +42,96 @@ def login():
         "client_id": DISCORD_CLIENT_ID,
         "redirect_uri": DISCORD_REDIRECT_URI,
         "response_type": "code",
-        "scope": "identify guilds guilds.members.read guilds.join role_connections.write"
+        "scope": "identify guilds guilds.members.read guilds.join role_connections.write",
     }
     return redirect(f"https://discord.com/api/oauth2/authorize?{urlencode(params)}")
+
 
 @app.route("/callback")
 def callback():
     error = request.args.get("error")
     if error:
         print(session, file=sys.stderr)
-        return render_template("index.html", img="images/index/blank.png", text="Log-in<br>with Discord", num=NUM)
-    
+        return render_template(
+            "index.html",
+            img="images/index/blank.png",
+            text="Log-in<br>with Discord",
+            num=NUM,
+        )
+
     code = request.args.get("code")
     if not code:
-        return 'Error: No code provided', 400
-    
+        return "Error: No code provided", 400
+
     token_data = {
         "grant_type": "authorization_code",
         "code": code,
         "redirect_uri": DISCORD_REDIRECT_URI,
         "client_id": DISCORD_CLIENT_ID,
-        "client_secret": DISCORD_CLIENT_SECRET
+        "client_secret": DISCORD_CLIENT_SECRET,
     }
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    response = requests.post("https://discord.com/api/oauth2/token", data=token_data, headers=headers)
-    
+    response = requests.post(
+        "https://discord.com/api/oauth2/token", data=token_data, headers=headers
+    )
+
     token = response.json()["access_token"]
     if not token:
-        return 'Error: No token received', 400
+        return "Error: No token received", 400
     session["token"] = token
-    
-    return redirect(url_for("profile"))
 
-@app.route("/profile")
-def profile():
-    token = session.get("token")
-    if not token:
-        return redirect(url_for("login"))
-    
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get("https://discord.com/api/users/@me", headers=headers)
     if response.status_code != 200:
-        return 'Error: No Response', 400
+        return "Error: No Response", 400
+
     user_data = response.json()
     if not user_data:
-        return 'Error: No user data received', 400
+        return "Error: No user data received", 400
     session["user_data"] = user_data
 
     user_id = user_data["id"]
     avatar_hash = user_data["avatar"]
     avatar_url = "images/index/blank.png"
     if avatar_hash:
-        file_type = ['png','gif'][avatar_hash.startswith("a_")]
-        avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.{file_type}"
+        file_type = ["png", "gif"][avatar_hash.startswith("a_")]
+        avatar_url = (
+            f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.{file_type}"
+        )
         session["user_data"]["img"] = avatar_url
 
     return render_template("index.html", img=avatar_url, text="Logout", num=NUM)
+
+
+@app.route("/challenge/<num>", methods=["GET", "POST"])
+def get_challenge(num):
+    if request.method == "GET":
+        img = session["user_data"]["img"] if "user_data" in session else "images/index/blank.png"
+        text = "Logout" if "user_data" in session else "Log-in<br>with Discord"
+
+        with open(os.path.join(current_app.static_folder, "data.json")) as f:
+            data = json.load(f)
+        try:
+            a, b = data[num].values()
+        except KeyError:
+            return render_template("index.html", img=img, text=text, num=NUM)
+        
+        params = {
+            "img": img,
+            "text": text,
+            "num":num,
+            "a": a,
+            "b": b,
+            "sol1": a["solution"],
+            "sol2": b["solution"],
+            "parttwo": True,
+            "done": True,
+        }
+        return render_template("challenge.html", **params)
+        
+
+    else:  # POST
+        return "Method not allowed", 405
 
 
 @app.route("/access")
@@ -107,41 +145,51 @@ def access():
     role_id = 1252507286489010178
     headers = {"Authorization": f"Bot {bot_token}", "Content-Type": "application/json"}
 
-    response = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}", headers=headers)
+    response = requests.get(
+        f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}",
+        headers=headers,
+    )
     if response.status_code == 404:  # User is not a member of the guild
         payload = {"access_token": session["token"]}
         try:
-            response = requests.put(f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}", headers=headers, json=payload)
+            response = requests.put(
+                f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}",
+                headers=headers,
+                json=payload,
+            )
             response.raise_for_status()
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}", file=sys.stderr)
             error_message = response.text
-            return f'Error: Failed to assign role: {error_message}', 400
+            return f"Error: Failed to assign role: {error_message}", 400
 
     try:
-        response = requests.put(f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}/roles/{role_id}", headers=headers)
+        response = requests.put(
+            f"https://discord.com/api/v9/guilds/{guild_id}/members/{user_id}/roles/{role_id}",
+            headers=headers,
+        )
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}", file=sys.stderr)
     else:
-        if response.status_code!= 204:
+        if response.status_code != 204:
             error_message = response.text
             try:
                 error_json = response.json()
-                error_message = error_json.get('message', error_message)
+                error_message = error_json.get("message", error_message)
             except ValueError as e:
                 print(f"Error: {e}", file=sys.stderr)
                 pass
-            return f'Error: Failed to assign role: {error_message}', 400
-    return 'Role assigned successfully!'
+            return f"Error: Failed to assign role: {error_message}", 400
+    return "Role assigned successfully!"
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('token', None)
-    session.pop('user_data', None)
-    return redirect(url_for('index'))
+    session.pop("token", None)
+    session.pop("user_data", None)
+    return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
