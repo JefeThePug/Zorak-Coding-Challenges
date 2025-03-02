b7468973ef23a489ed3443731ece6e6123ff900b
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2025-03-02 16:29:02 +0900

    Confetti Fix
    Only trigger confetti on week 10

--- website/static/js/ending.js
+++ website/static/js/ending.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("confetti-canvas");
   const duration = 6 * 1000;
   let animationEnd = Date.now() + duration;
   const defaults = {
@@ -11,24 +12,25 @@ document.addEventListener("DOMContentLoaded", function () {
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
   }
+  if (canvas) {
+    let interval = setInterval(function () {
+      let timeLeft = animationEnd - Date.now();
 
-  let interval = setInterval(function () {
-    let timeLeft = animationEnd - Date.now();
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
 
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
-    }
-
-    let particleCount = 50 * (timeLeft / duration);
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-    });
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-    });
-  }, 250);
+      let particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2},
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2},
+      });
+    }, 250);
+  }
 });
