f30b9c33a59de044a5c18eb2a710212550e11905
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2025-03-09 12:13:20 +0100

    Drop this because unnecessary

--- db/init.sql
+++
@@ -1,7 +0,0 @@
--- Here, we can specify any SQL we want executed on database startup
--- by default, postgres first off any init.sql script that
--- is located at /docker-entrypoint-initdb.d/init.sql
--- We copy this script to that location inside the postgres docker container.
-
--- For example, we may use this script to "seed" the database with some data
--- we can create databases, add data... whatever we need to do.
