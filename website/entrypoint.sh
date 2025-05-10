4bb6a62034c555d067e2b4a7d5036d95dc7ffefc
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2025-05-10 15:56:11 +0200

    Added gunicorn

--- website/entrypoint.sh
+++ website/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # This script runs when the api container starts up.
-python setup.py ${DISCORD_ADMIN_USER_ID} && python app.py
+
+python setup.py "${DISCORD_ADMIN_USER_ID}"
+
+# Replace 'your_module:app' with the actual path to your Flask app instance
+exec gunicorn --bind 0.0.0.0:5000 app:app
